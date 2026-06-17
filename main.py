163aed2e112262402e8749425b262d7d52424b39
Gaia Rossi <rssgai@unife.it>
2026-06-17 16:48:04 +0200

    fix reconnect logic and add restart endpoint

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def setup_logging():
 
     sh = logging.StreamHandler()
     sh.setFormatter(fmt)
-    sh.setLevel(getattr(logging, shell_level.upper(), logging.INFO))
+    sh.setLevel(getattr(logging, shell_level.upper(), logging.DEBUG))
     root.addHandler(sh)
 
     log_process = logging.getLogger("process")
@@ -151,7 +151,8 @@ processing = Processing(
 )
 
 startup_mqtt_connection = int(os.environ.get("STARTUP_MQTT_CONNECTION", 1))
-mqtt_t: threading.Thread | None = None
+mqtt_t: threading.Thread
+processing_t: threading.Thread
 
 @app.route("/rebuild", methods=["POST"])
 def rebuild():
@@ -192,20 +193,21 @@ def disconnect():
 
 @app.route("/reconnect", methods=["POST"])
 def reconnect():
-    global mqtt_t
+    global mqtt_t, mqtt_connection
     try:
         if mqtt_t and mqtt_t.is_alive():
+            mqtt_connection.stop()
             mqtt_t.join()
-        mqtt_connection.mqtt_loop_run = True
-        mqtt_t = threading.Thread(target=mqtt_connection.run)
-        mqtt_t.start()
-        reconnection_time = time.time()
-        mqtt_active_ts.set(reconnection_time)
-        logger.info(f"Reconnecting to mqtt. Time: {reconnection_time}")
     except:
-        return jsonify({"status": "error"})
+        pass
 
-    return jsonify({"status": "success"})
+    mqtt_connection.reset()
+    mqtt_t = threading.Thread(target=mqtt_connection.run)
+    mqtt_t.start()
+    reconnection_time = time.time()
+    mqtt_active_ts.set(reconnection_time)
+    logger.info(f"Reconnecting to mqtt. Time: {reconnection_time}")
+    return jsonify({"status": "success"}), 200
 
 
 @app.route("/healthd")
@@ -238,14 +240,53 @@ def reset_logging_overhead():
     mqtt_connection.reset_logging_overhead_buffer()
     return jsonify({"status": "success"})
 
-@app.route("/mongo_rebuild_size", methods=["POST"])
-def set_mongo_rebuild_size():
-    mongo_messages_no = request.get_json().get("mongo_messages_no", None)
-    if mongo_messages_no is None:
-        return jsonify({"status": "ok", "message": "no mongo messages number provided, not updating."})
-    else:
-        processing.set_mongo_rebuild_size(mongo_messages_no)
-        return jsonify({"status": "ok", "message": f"mongo rebuild size updated to {mongo_messages_no}."})
+@app.route("/restart", methods=["POST"])
+def restart():
+    global mqtt_t, processing_t, mqtt_connection, processing, processing_buffer, connection_buffer, messages_buffer
+    mqtt_connection.stop()
+    processing.stop()
+
+    try:
+        if mqtt_t and mqtt_t.is_alive():
+            mqtt_t.join()
+    except:
+        pass
+
+    try:
+        if processing_t and processing_t.is_alive():
+            processing_t.join()
+    except:
+        pass
+
+    connection_buffer.clear()
+    processing_buffer.clear()
+    messages_buffer.clear()
+
+    mqtt_connection = MqttConnection(
+        connection_buffer=connection_buffer,
+        mqtt_conf=connection_mqtt_conf,
+        mongo_url=connection_mongo_url,
+        mongo_db=connection_mongo_db,
+        mongo_collection=connection_mongo_collection,
+        messages_buffer=messages_buffer,
+        random_seed=random_seed
+    )
+    processing = Processing(
+        connection_buffer=connection_buffer,
+        processing_buffer=processing_buffer,
+        state_max_size=state_max_size,
+        worker=process_burn_worker,
+        work=process_burn_work,
+        mongo_url=connection_mongo_url,
+        mongo_db=connection_mongo_db,
+        mongo_collection=connection_mongo_collection,
+        messages_buffer=messages_buffer,
+    )
+    processing_t = threading.Thread(target=processing.run)
+
+    processing_t.start()
+    logger.debug("Processing restarted.")
+    return jsonify({"status": "restart success with no mqtt connection. Call reconnect to start mqtt connection."})
 
 if __name__ == "__main__":
     logger.debug("Started main.")
